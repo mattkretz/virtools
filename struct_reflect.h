da6d680ba4f421e7829c8d8baaa80ee961a2b111
Matthias Kretz <kretz@kde.org>
2020-02-11 14:30:38 +0100

    Update struct_reflect to be more robust and precise
    * Make use of C++20 concepts
    * Require __cpp_concepts and __cpp_structured_bindings
    * Be more precise in requiring types that can be destructured
    * Fix ref- and cv-qualifiers on tuple template parameters, supporting a
      forwarding (forward_as_tuple) and copy use-case via two different
      functions.
    * Support destructuring into std::pair for 2-element structures.
    * Add meta-functions that turn a type T that can be destructured into a
      tuple or pair.

--- struct_reflect.h
+++ struct_reflect.h
@@ -27,70 +27,208 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef VIR_STRUCT_SIZE_H_
 #define VIR_STRUCT_SIZE_H_
+#if defined __cpp_structured_bindings && defined __cpp_concepts && __cpp_concepts >= 201907
 #include <utility>
 #include <tuple>
-
-#ifdef __cpp_structured_bindings
-#if !(defined __clang_major__ && __clang_major__ == 5 && __clang_minor__ == 0 &&         \
-      __clang_patchlevel__ < 1)  // clang 5.0.0 breaks on structured bindings
-#define VIR_HAVE_STRUCT_GET 1
-#endif
-#endif
 
 namespace vir
 {
 namespace detail  //{{{1
 {
+using std::remove_cvref_t;
 using std::size_t;
 
 // struct_size implementation {{{2
-struct anything {
-  template <class T> operator T();
+template <class Struct> struct anything_but_base_of {
+  template <class T>
+  requires(!std::is_base_of_v<T, Struct>)
+  operator T();
 };
 
-template <class T, size_t... Indexes, class = decltype(T{((void)Indexes, anything())...})>
-constexpr size_t struct_size(std::index_sequence<Indexes...>, int)
+template <class Struct> struct any_empty_base_of {
+  template <class T>
+  requires(std::is_base_of_v<T, Struct> && std::is_empty_v<T>)
+  operator T();
+};
+
+#define VIR_DEFINE_TEST_FUNCTIONS_(size_, ...)                                           \
+  template <class T> constexpr auto test_##size_(const T *obj = nullptr)                 \
+  {                                                                                      \
+    [[maybe_unused]] const auto &[__VA_ARGS__] = *obj;                                   \
+  }
+VIR_DEFINE_TEST_FUNCTIONS_(26, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
+VIR_DEFINE_TEST_FUNCTIONS_(25, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y);
+VIR_DEFINE_TEST_FUNCTIONS_(24, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x);
+VIR_DEFINE_TEST_FUNCTIONS_(23, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w);
+VIR_DEFINE_TEST_FUNCTIONS_(22, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v);
+VIR_DEFINE_TEST_FUNCTIONS_(21, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u);
+VIR_DEFINE_TEST_FUNCTIONS_(20, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t);
+VIR_DEFINE_TEST_FUNCTIONS_(19, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s);
+VIR_DEFINE_TEST_FUNCTIONS_(18, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r);
+VIR_DEFINE_TEST_FUNCTIONS_(17, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q);
+VIR_DEFINE_TEST_FUNCTIONS_(16, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
+VIR_DEFINE_TEST_FUNCTIONS_(15, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o);
+VIR_DEFINE_TEST_FUNCTIONS_(14, a, b, c, d, e, f, g, h, i, j, k, l, m, n);
+VIR_DEFINE_TEST_FUNCTIONS_(13, a, b, c, d, e, f, g, h, i, j, k, l, m);
+VIR_DEFINE_TEST_FUNCTIONS_(12, a, b, c, d, e, f, g, h, i, j, k, l);
+VIR_DEFINE_TEST_FUNCTIONS_(11, a, b, c, d, e, f, g, h, i, j, k);
+VIR_DEFINE_TEST_FUNCTIONS_(10, a, b, c, d, e, f, g, h, i, j);
+VIR_DEFINE_TEST_FUNCTIONS_( 9, a, b, c, d, e, f, g, h, i);
+VIR_DEFINE_TEST_FUNCTIONS_( 8, a, b, c, d, e, f, g, h);
+VIR_DEFINE_TEST_FUNCTIONS_( 7, a, b, c, d, e, f, g);
+VIR_DEFINE_TEST_FUNCTIONS_( 6, a, b, c, d, e, f);
+VIR_DEFINE_TEST_FUNCTIONS_( 5, a, b, c, d, e);
+VIR_DEFINE_TEST_FUNCTIONS_( 4, a, b, c, d);
+VIR_DEFINE_TEST_FUNCTIONS_( 3, a, b, c);
+VIR_DEFINE_TEST_FUNCTIONS_( 2, a, b);
+VIR_DEFINE_TEST_FUNCTIONS_( 1, a);
+#undef VIR_DEFINE_TEST_FUNCTIONS_
+
+template <class T, size_t... Indexes>
+concept brace_constructible =
+(
+   requires { T{((void)Indexes, anything_but_base_of<T>())...}; }
+|| requires { T{any_empty_base_of<T>(), ((void)Indexes, anything_but_base_of<T>())...}; }
+|| requires { T{any_empty_base_of<T>(), any_empty_base_of<T>(), ((void)Indexes, anything_but_base_of<T>())...}; }
+|| requires { T{any_empty_base_of<T>(), any_empty_base_of<T>(), any_empty_base_of<T>(), ((void)Indexes, anything_but_base_of<T>())...}; }
+) && (
+   (sizeof...(Indexes) ==  1 && requires { test_1 <T>(); })
+|| (sizeof...(Indexes) ==  2 && requires { test_2 <T>(); }) 
+|| (sizeof...(Indexes) ==  3 && requires { test_3 <T>(); }) 
+|| (sizeof...(Indexes) ==  4 && requires { test_4 <T>(); }) 
+|| (sizeof...(Indexes) ==  5 && requires { test_5 <T>(); }) 
+|| (sizeof...(Indexes) ==  6 && requires { test_6 <T>(); }) 
+|| (sizeof...(Indexes) ==  7 && requires { test_7 <T>(); }) 
+|| (sizeof...(Indexes) ==  8 && requires { test_8 <T>(); }) 
+|| (sizeof...(Indexes) ==  9 && requires { test_9 <T>(); }) 
+|| (sizeof...(Indexes) == 10 && requires { test_10<T>(); }) 
+|| (sizeof...(Indexes) == 11 && requires { test_11<T>(); }) 
+|| (sizeof...(Indexes) == 12 && requires { test_12<T>(); }) 
+|| (sizeof...(Indexes) == 13 && requires { test_13<T>(); }) 
+|| (sizeof...(Indexes) == 14 && requires { test_14<T>(); }) 
+|| (sizeof...(Indexes) == 15 && requires { test_15<T>(); }) 
+|| (sizeof...(Indexes) == 16 && requires { test_16<T>(); }) 
+|| (sizeof...(Indexes) == 17 && requires { test_17<T>(); }) 
+|| (sizeof...(Indexes) == 18 && requires { test_18<T>(); }) 
+|| (sizeof...(Indexes) == 19 && requires { test_19<T>(); }) 
+|| (sizeof...(Indexes) == 20 && requires { test_20<T>(); }) 
+|| (sizeof...(Indexes) == 21 && requires { test_21<T>(); }) 
+|| (sizeof...(Indexes) == 22 && requires { test_22<T>(); }) 
+|| (sizeof...(Indexes) == 23 && requires { test_23<T>(); })
+|| (sizeof...(Indexes) == 24 && requires { test_24<T>(); })
+|| (sizeof...(Indexes) == 25 && requires { test_25<T>(); })
+|| (sizeof...(Indexes) == 26 && requires { test_26<T>(); })
+);
+
+template <class T, size_t... Indexes>
+requires brace_constructible<T, Indexes...>
+constexpr size_t struct_size(std::index_sequence<Indexes...>)
 {
   return sizeof...(Indexes);
 }
 
-template <class T, size_t... Indexes>
-constexpr size_t struct_size(std::index_sequence<Indexes...>, float)
+template <class T>
+requires requires { T{}; }
+constexpr size_t struct_size(std::index_sequence<>)
 {
-  static_assert(sizeof...(Indexes) > 0,
+  static_assert(std::is_empty_v<T>,
                 "Increase MaxSize on your struct_size call. (Or you found a bug)");
-  return struct_size<T>(std::make_index_sequence<sizeof...(Indexes) - 1>(), int());
+  return 0;
+}
+
+template <class T, size_t I0, size_t... Indexes>
+requires(!brace_constructible<T, I0, Indexes...>)
+constexpr size_t struct_size(std::index_sequence<I0, Indexes...>)
+{
+  return struct_size<T>(std::index_sequence<Indexes...>());
 }
 
 // struct_get implementation {{{2
 template <size_t Total> struct struct_get;
 
-#ifdef VIR_HAVE_STRUCT_GET
+template <typename T> struct remove_ref_in_tuple;
+template <typename... Ts> struct remove_ref_in_tuple<std::tuple<Ts...>> {
+  using type = std::tuple<std::remove_reference_t<Ts>...>;
+};
+template <typename T1, typename T2> struct remove_ref_in_tuple<std::pair<T1, T2>> {
+  using type = std::pair<std::remove_reference_t<T1>, std::remove_reference_t<T2>>;
+};
+
 template <> struct struct_get<1> {
-  template <class T> auto as_tuple(T &&obj)
+  template <class T> auto to_tuple_ref(T &&obj)
   {
-    auto && [a] = std::forward<T>(obj);
-    return std::tie(a);
+    auto && [a] = obj;
+    return std::forward_as_tuple(a);
+  }
+  template <class T>
+  auto to_tuple(const T &obj) ->
+      typename remove_ref_in_tuple<decltype(to_tuple_ref(std::declval<T>()))>::type
+  {
+    const auto &[a] = obj;
+    return {a};
+  }
+  template <size_t N, class T> const auto &get(const T &obj)
+  {
+    auto && [a] = obj;
+    return a;
+  }
+  template <size_t N, class T> auto &get(T &obj)
+  {
+    auto && [a] = obj;
+return a;
+  }
+};
+
+template <> struct struct_get<2> {
+  template <class T> auto to_tuple_ref(T &&obj)
+  {
+    auto &&[a, b] = obj;
+    return std::forward_as_tuple(a, b);
+  }
+  template <class T>
+  auto to_tuple(const T &obj) ->
+      typename remove_ref_in_tuple<decltype(to_tuple_ref(std::declval<T>()))>::type
+  {
+    const auto &[a, b] = obj;
+    return {a, b};
+  }
+  template <class T> auto to_pair_ref(T &&obj)
+  {
+    auto &&[a, b] = obj;
+    return std::pair<decltype((a)), decltype((b))>(a, b);
+  }
+  template <class T>
+  auto to_pair(const T &obj) ->
+      typename remove_ref_in_tuple<decltype(to_pair_ref(std::declval<T>()))>::type
+  {
+    const auto &[a, b] = obj;
+    return {a, b};
   }
   template <size_t N, class T> auto &get(T &&obj)
   {
-    auto && [a] = std::forward<T>(obj);
-    auto &b = a;
-    return b;
+    auto &&[a, b] = obj;
+    return std::get<N>(std::forward_as_tuple(a, b));
   }
 };
 
 #define VIR_STRUCT_GET_(size_, ...)                                                      \
   template <> struct struct_get<size_> {                                                 \
-    template <class T> auto as_tuple(T &&obj)                                            \
+    template <class T> auto to_tuple_ref(T &&obj)                                        \
     {                                                                                    \
-      auto && [__VA_ARGS__] = std::forward<T>(obj);                                      \
-      return std::tie(__VA_ARGS__);                                                      \
+      auto &&[__VA_ARGS__] = obj;                                                        \
+      return std::forward_as_tuple(__VA_ARGS__);                                         \
+    }                                                                                    \
+    template <class T>                                                                   \
+    auto to_tuple(const T &obj) ->                                                       \
+        typename remove_ref_in_tuple<decltype(to_tuple_ref(std::declval<T>()))>::type    \
+    {                                                                                    \
+      const auto &[__VA_ARGS__] = obj;                                                   \
+      return {__VA_ARGS__};                                                              \
     }                                                                                    \
     template <size_t N, class T> auto &get(T &&obj)                                      \
     {                                                                                    \
-      auto && [__VA_ARGS__] = std::forward<T>(obj);                                      \
-      return std::get<N>(std::tie(__VA_ARGS__));                                         \
+      auto &&[__VA_ARGS__] = obj;                                                        \
+      return std::get<N>(std::forward_as_tuple(__VA_ARGS__));                            \
     }                                                                                    \
   }
 VIR_STRUCT_GET_(26, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
@@ -117,54 +255,148 @@ VIR_STRUCT_GET_( 6, a, b, c, d, e, f);
 VIR_STRUCT_GET_( 5, a, b, c, d, e);
 VIR_STRUCT_GET_( 4, a, b, c, d);
 VIR_STRUCT_GET_( 3, a, b, c);
-VIR_STRUCT_GET_( 2, a, b);
 #undef VIR_STRUCT_GET_
-#endif  // VIR_HAVE_STRUCT_GET
 
+// concept definitions {{{2
+template <typename T> concept has_tuple_size = requires(T x, std::tuple_size<T> ts)
+{
+  {static_cast<int>(ts)} -> std::same_as<int>;
+  struct_get<std::tuple_size_v<T>>().to_tuple(x);
+};
+template <typename T>
+concept aggregate_without_tuple_size = std::is_aggregate_v<T> && !has_tuple_size<T>;
+
+// traits {{{2
+template <typename From, typename To> struct copy_cvref {
+  using From2 = std::remove_reference_t<From>;
+  using with_const = std::conditional_t<std::is_const_v<From2>, const To, To>;
+  using with_volatile =
+      std::conditional_t<std::is_volatile_v<From2>, volatile with_const, with_const>;
+  using type = std::conditional_t<std::is_lvalue_reference_v<From>, with_volatile &,
+                                  std::conditional_t<std::is_rvalue_reference_v<From>,
+                                                     with_volatile &&, with_volatile>>;
+};
 }  // namespace detail }}}1
 
 /**
  * The value of struct_size is the number of non-static data members of the type \p T.
+ * More precisely, struct_size is the number of elements in the identifier-list of a
+ * structured binding declaration.
  *
- * \tparam T A standard-layout structure type.
+ * \tparam T An aggregate type or a type specializing `std::tuple_size` that can be
+ *           destructured via a structured binding. This implies that either \p T has only
+ *           empty bases or \p T has no non-static data members and a single base class
+ *           with non-static data members. The uses of aggregate types that do not support
+ *           destructuring is ill-formed. The uses of non-aggregate types that do not
+ *           support destructuring results in a substitution failure.
+ *
+ * \tparam MaxSize The maximum number of non-static data members to expect. The
+ *                 implementation uses an upper limit of 26.
  */
-template <class T, std::size_t MaxSize = sizeof(T),
-          class = typename std::enable_if<std::is_class<T>::value &&
-                                          std::is_standard_layout<T>::value>::type>
-constexpr std::size_t struct_size =
-    detail::struct_size<T>(std::make_index_sequence<MaxSize>(), int());
+template <typename T, std::size_t MaxSize = sizeof(T)>
+requires detail::aggregate_without_tuple_size<T> || detail::has_tuple_size<T>
+constexpr inline std::size_t struct_size;
+
+template <detail::aggregate_without_tuple_size T, std::size_t MaxSize>
+constexpr inline std::size_t struct_size<T, MaxSize> =
+    detail::struct_size<T>(std::make_index_sequence<std::min(std::size_t(26), MaxSize)>());
+
+template <detail::has_tuple_size T, std::size_t MaxSize>
+constexpr inline std::size_t struct_size<T, MaxSize> = std::tuple_size_v<T>;
 
 /**
  * Returns a cv-qualified reference to the \p N -th non-static data member in \p obj.
  */
-template <std::size_t N, class T, std::size_t Total = struct_size<std::decay_t<T>>,
+template <std::size_t N, class T,
+          std::size_t Total = struct_size<std::remove_cvref_t<T>>,
           class = std::enable_if_t<(N < Total)>>
-auto &struct_get(T &&obj)
+decltype(auto) struct_get(T &&obj)
 {
-#ifndef VIR_HAVE_STRUCT_GET
-  static_assert(N > Total, "struct_get requires structured bindings (C++17)");
-#endif
   return detail::struct_get<Total>().template get<N>(std::forward<T>(obj));
 }
 
 /**
- * An alias for the type of the \p N -th non-static data member of \p T.
+ * `struct_element_t` is an alias for the type of the \p N -th non-static data member of
+ * \p T.
  */
 template <std::size_t N, class T>
 using struct_element_t =
     std::remove_reference_t<decltype(struct_get<N>(std::declval<T &>()))>;
 
 /**
+ * Returns a std::tuple with a copy of all the non-static data members of \p obj.
+ */
+template <class T, std::size_t Total = struct_size<std::remove_cvref_t<T>>>
+auto to_tuple(T &&obj)
+{
+  return detail::struct_get<Total>().to_tuple(std::forward<T>(obj));
+}
+
+/**
  * Returns a std::tuple of lvalue references to all the non-static data members of \p obj.
  */
-template <class T, std::size_t Total = struct_size<std::decay_t<T>>>
-auto as_tuple(T &&obj)
+template <class T, std::size_t Total = struct_size<std::remove_cvref_t<T>>>
+auto to_tuple_ref(T &&obj)
 {
-  return detail::struct_get<Total>().as_tuple(std::forward<T>(obj));
+  return detail::struct_get<Total>().to_tuple_ref(std::forward<T>(obj));
 }
+
+/**
+ * Defines the member type \p type to a std::tuple specialization matching the non-static
+ * data members of \p T.
+ */
+template <class T>
+struct as_tuple
+    : detail::copy_cvref<
+          T, decltype(detail::struct_get<struct_size<std::remove_cvref_t<T>>>().to_tuple(
+                 std::declval<T>()))> {
+};
+
+/**
+ * Alias for a std::tuple specialization matching the non-static data members of \p T.
+ */
+template <class T> using as_tuple_t = typename as_tuple<T>::type;
+
+/**
+ * Returns a std::pair with a copy of all the non-static data members of \p obj.
+ */
+template <class T>
+requires(struct_size<std::remove_cvref_t<T>> == 2)
+auto to_pair(T &&obj)
+{
+  return detail::struct_get<2>().to_pair(std::forward<T>(obj));
+}
+
+/**
+ * Returns a std::pair of lvalue references to all the non-static data members of \p obj.
+ */
+template <class T>
+requires(struct_size<std::remove_cvref_t<T>> == 2)
+auto to_pair_ref(T &&obj)
+{
+  return detail::struct_get<2>().to_pair_ref(std::forward<T>(obj));
+}
+
+/**
+ * Defines the member type \p type to a std::pair specialization matching the non-static
+ * data members of \p T.
+ */
+template <class T>
+requires (struct_size<std::remove_cvref_t<T>> == 2)
+struct as_pair
+    : detail::copy_cvref<
+          T, decltype(detail::struct_get<2>().to_pair(
+                 std::declval<T>()))> {
+};
+
+/**
+ * Alias for a std::pair specialization matching the non-static data members of \p T.
+ */
+template <class T> using as_pair_t = typename as_pair<T>::type;
 
 }  // namespace vir
 
+#endif // structured bindings & concepts
 #endif  // VIR_STRUCT_SIZE_H_
 
 // vim: foldmethod=marker
--- tests/struct_reflect.cpp
+++ tests/struct_reflect.cpp
@@ -52,7 +52,7 @@ struct D {
 struct E {
   A a;
   volatile B b;
-  const C c;
+  const C &c;
   const volatile D d;
 };
 
@@ -63,57 +63,110 @@ struct F {
   const int *const d;
 };
 
+using G = int[3];
+
+struct H1 { float a, b, c; };
+struct H2 : public H1 {};
+struct Empty1 {};
+struct Empty2 {};
+struct H3 : Empty1, Empty2 { int a, b; };
+struct H4 { H1 a, b; };
+struct H5 { Empty1 a, b; };
+
 TEST(struct_size)
 {
+  COMPARE(vir::struct_size<Empty1>, 0u);
   COMPARE(vir::struct_size<A>, 1u);
   COMPARE(vir::struct_size<B>, 1u);
   COMPARE(vir::struct_size<C>, 1u);
   COMPARE(vir::struct_size<D>, 1u);
   COMPARE(vir::struct_size<E>, 4u);
   COMPARE(vir::struct_size<F>, 4u);
+  COMPARE(vir::struct_size<G>, 3u);
+  COMPARE(vir::struct_size<H1>, 3u);
+  COMPARE(vir::struct_size<H2>, 3u);
+  COMPARE(vir::struct_size<H3>, 2u);
+  COMPARE(vir::struct_size<H4>, 2u);
+  COMPARE(vir::struct_size<H5>, 2u);
+  COMPARE((vir::struct_size<std::pair<int, float>>), 2u);
+  COMPARE((vir::struct_size<std::tuple<int, float, int>>), 3u);
 }
 
-#ifdef VIR_HAVE_STRUCT_GET
+template <typename T> struct Foo {};
+
+TEST(as_tuple_t)
+{
+  COMPARE_TYPES(vir::as_tuple_t<A>, std::tuple<int>);
+  COMPARE_TYPES(vir::as_tuple_t<const A>, const std::tuple<int>);
+  COMPARE_TYPES(vir::as_tuple_t<B>, std::tuple<std::array<int, 2>>);
+  COMPARE_TYPES(vir::as_tuple_t<C>, std::tuple<decltype(C().c)>);
+  COMPARE_TYPES(vir::as_tuple_t<D>, std::tuple<decltype(D().c)>);
+  COMPARE_TYPES(vir::as_tuple_t<F>, std::tuple<int *, const int *, int *const, const int *const>);
+  COMPARE_TYPES(vir::as_tuple_t<const F>, const std::tuple<int *, const int *, int *const, const int *const>);
+  COMPARE_TYPES(vir::as_tuple_t<std::pair<int, const int>>, std::tuple<int, const int>);
+  COMPARE_TYPES(decltype(vir::to_tuple_ref(std::pair<int, const int>())), std::tuple<int&, const int&>);
+}
+
+TEST(as_pair_t)
+{
+  COMPARE_TYPES(vir::as_pair_t<H3>, std::pair<int, int>);
+  COMPARE_TYPES(vir::as_pair_t<H4>, std::pair<H1, H1>);
+  COMPARE_TYPES(vir::as_pair_t<H5>, std::pair<Empty1, Empty1>);
+}
+
 TEST(struct_get)
 {
-  COMPARE(typeid(vir::struct_get<0>(A())), typeid(const int &));
-  COMPARE(typeid(vir::struct_get<0>(B())), typeid(const std::array<int, 2> &));
+  COMPARE_TYPES(decltype(vir::struct_get<0>(A())), const int&);
+  COMPARE_TYPES(decltype(vir::struct_get<0>(std::declval<A&>())), int&);
+  COMPARE_TYPES(decltype(vir::struct_get<0>(std::declval<A&>())), int&);
+  COMPARE_TYPES(decltype(vir::struct_get<0>(B())), const std::array<int, 2> &);
   A a;
-  COMPARE(typeid(vir::struct_get<0>(a)), typeid(int &));
+  COMPARE_TYPES(decltype(vir::struct_get<0>(a)), int &);
 
   E e{{}, {}, {}, {}};
   const E &ce = e;
-  COMPARE(typeid(vir::struct_get<0>(ce)), typeid(const A &));
-  COMPARE(typeid(vir::struct_get<1>(ce)), typeid(const volatile B &));
-  COMPARE(typeid(vir::struct_get<2>(ce)), typeid(const C &));
-  COMPARE(typeid(vir::struct_get<3>(ce)), typeid(const volatile D &));
+  COMPARE_TYPES(decltype(vir::struct_get<0>(ce)), const A &);
+  COMPARE_TYPES(decltype(vir::struct_get<1>(ce)), const volatile B &);
+  COMPARE_TYPES(decltype(vir::struct_get<2>(ce)), const C &);
+  COMPARE_TYPES(decltype(vir::struct_get<3>(ce)), const volatile D &);
 
-  COMPARE(typeid(vir::struct_get<0>(e)), typeid(A &));
-  COMPARE(typeid(vir::struct_get<1>(e)), typeid(volatile B &));
-  COMPARE(typeid(vir::struct_get<2>(e)), typeid(const C &));
-  COMPARE(typeid(vir::struct_get<3>(e)), typeid(const volatile D &));
+  COMPARE_TYPES(decltype(vir::struct_get<0>(e)), A &);
+  COMPARE_TYPES(decltype(vir::struct_get<1>(e)), volatile B &);
+  COMPARE_TYPES(decltype(vir::struct_get<2>(e)), const C &);
+  COMPARE_TYPES(decltype(vir::struct_get<3>(e)), const volatile D &);
+
+  H2 h2{};
+  COMPARE_TYPES(decltype(vir::struct_get<0>(h2)), float &);
+
+  //H3 h3{};
+  //COMPARE_TYPES(decltype(vir::struct_get<0>(h3)), int &);
+  //COMPARE_TYPES(decltype(vir::struct_get<1>(h3)), int &);
 }
 
 TEST(struct_element)
 {
-  COMPARE(typeid(vir::struct_element_t<0, E>), typeid(A));
-  COMPARE(typeid(vir::struct_element_t<1, E>), typeid(volatile B));
-  COMPARE(typeid(vir::struct_element_t<2, E>), typeid(const C));
-  COMPARE(typeid(vir::struct_element_t<3, E>), typeid(const volatile D));
+  COMPARE_TYPES(vir::struct_element_t<0, E>, A);
+  COMPARE_TYPES(vir::struct_element_t<1, E>, volatile B);
+  COMPARE_TYPES(vir::struct_element_t<2, E>, const C);
+  COMPARE_TYPES(vir::struct_element_t<3, E>, const volatile D);
 
-  COMPARE(typeid(vir::struct_element_t<0, F>), typeid(int *));
-  COMPARE(typeid(vir::struct_element_t<1, F>), typeid(const int *));
-  COMPARE(typeid(vir::struct_element_t<2, F>), typeid(int *const));
-  COMPARE(typeid(vir::struct_element_t<3, F>), typeid(const int *const));
+  COMPARE_TYPES(vir::struct_element_t<0, F>, int *);
+  COMPARE_TYPES(vir::struct_element_t<1, F>, const int *);
+  COMPARE_TYPES(vir::struct_element_t<2, F>, int *const);
+  COMPARE_TYPES(vir::struct_element_t<3, F>, const int *const);
+
+  COMPARE_TYPES(vir::struct_element_t<0, G>, int);
+  COMPARE_TYPES(vir::struct_element_t<1, G>, int);
+  COMPARE_TYPES(vir::struct_element_t<2, G>, int);
 }
 
-TEST(as_tuple)
+TEST(to_tuple)
 {
   {
     A a{1};
-    std::tuple<int> tup_a1 = vir::as_tuple(a);
-    std::tuple<int &> tup_a2 = vir::as_tuple(a);
-    std::tuple<const int &> tup_a3 = vir::as_tuple(a);
+    std::tuple<int> tup_a1 = vir::to_tuple(a);
+    std::tuple<int &> tup_a2 = vir::to_tuple_ref(a);
+    std::tuple<const int &> tup_a3 = vir::to_tuple_ref(a);
     COMPARE(a.a, 1);
     COMPARE(std::get<0>(tup_a1), 1);
     COMPARE(std::get<0>(tup_a2), 1);
@@ -132,20 +185,81 @@ TEST(as_tuple)
 
   {
     const A a{2};
-    std::tuple<int> tup1 = vir::as_tuple(a);
-    std::tuple<const int &> tup2 = vir::as_tuple(a);
+    std::tuple<int> tup1 = vir::to_tuple(a);
+    std::tuple<const int &> tup2 = vir::to_tuple(a);
     COMPARE(std::get<0>(tup1), 2);
     COMPARE(std::get<0>(tup2), 2);
-    COMPARE(typeid(std::tuple_element_t<0, decltype(vir::as_tuple(a))>), typeid(int));
-    COMPARE(typeid(vir::as_tuple(a)), typeid(std::tuple<const int &>));
+    COMPARE_TYPES(std::tuple_element_t<0, decltype(vir::to_tuple(a))>, int);
+    COMPARE_TYPES(decltype(vir::to_tuple(a)), std::tuple<int>);
+    COMPARE_TYPES(decltype(vir::to_tuple_ref(a)), std::tuple<const int &>);
   }
 
   {
     F f{0, 0, 0, 0};
     int tmp = 1;
-    std::get<0>(vir::as_tuple(f)) = &tmp;
+    std::get<0>(vir::to_tuple_ref(f)) = &tmp;
     COMPARE(f.a, &tmp);
-    COMPARE(std::get<0>(vir::as_tuple(f)), &tmp);
+    COMPARE(std::get<0>(vir::to_tuple(f)), &tmp);
+  }
+
+  {
+    G g{0, 2, 3};
+    std::get<0>(vir::to_tuple_ref(g)) = 1;
+    COMPARE(g[0], 1);
+    const auto &[a, b, c] = vir::to_tuple_ref(g);
+    COMPARE(a, 1);
+    COMPARE(b, 2);
+    COMPARE(c, 3);
+    g[0] = 4;
+    g[1] = 5;
+    g[2] = 6;
+    COMPARE(a, 4);
+    COMPARE(b, 5);
+    COMPARE(c, 6);
+  }
+
+  {
+    H3 h3{{}, {}, 1, 2};
+    auto [a, b] = vir::to_tuple_ref(h3);
+    COMPARE(a, 1);
+    a = 3;
+    COMPARE(h3.a, 3);
+    COMPARE(b, 2);
+    COMPARE_TYPES(decltype(a), int &);
+    COMPARE_TYPES(decltype(b), int &);
+    const H3& cref = h3;
+    auto [c, d] = vir::to_tuple_ref(cref);
+    auto [e, f] = vir::to_tuple(h3);
+    COMPARE(c, 3);
+    COMPARE(e, 3);
+    a = 1;
+    COMPARE(c, 1);
+    COMPARE(e, 3);
+    COMPARE(d, 2);
+    COMPARE(f, 2);
   }
 }
-#endif  // VIR_HAVE_STRUCT_GET
+
+TEST(to_pair)
+{
+  H3 h3{{}, {}, 1, 2};
+  COMPARE_TYPES(decltype(vir::to_pair_ref(h3)), std::pair<int&, int&>);
+  auto [a, b] = vir::to_pair_ref(h3);
+  COMPARE(a, 1);
+  a = 3;
+  COMPARE(h3.a, 3);
+  COMPARE(b, 2);
+  COMPARE_TYPES(decltype(a), int &);
+  COMPARE_TYPES(decltype(b), int &);
+  const H3 &cref = h3;
+  auto [c, d] = vir::to_pair_ref(cref);
+  COMPARE_TYPES(decltype(vir::to_pair_ref(cref)), std::pair<const int&, const int&>);
+  auto [e, f] = vir::to_pair(h3);
+  COMPARE(c, 3);
+  COMPARE(e, 3);
+  a = 1;
+  COMPARE(c, 1);
+  COMPARE(e, 3);
+  COMPARE(d, 2);
+  COMPARE(f, 2);
+}
